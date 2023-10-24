cff9dddf8a3f477702106f287e30637972974ad3
Egor <tushev.ea@yandex.com>
2023-10-24 19:43:45 +0300

    Initial Commit

---
+++ restaurant-tushev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  restaurant-tushev
+//
+//  Created by Egor Tushev on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
